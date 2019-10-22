35460c6ac541c4995dd84f091429be696b9c14f7
Ku Hung-Hsi <justlove0714@hotmail.com>
2019-10-23 07:51:41 +0800

    WIP

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pickle
 import os.path
+from pprint import pprint
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -62,99 +63,85 @@ def main():
     '''
     create by google drive
     '''
-    # sheet = gd.files().create(body={
-    #     'name': 'a new spreadsheet',
-    #     'parents': [folder['id']],
-    #     'description': 'a example worksheet',
-    #     'mimeType': "application/vnd.google-apps.spreadsheet"
-    # }).execute()
-    # print(sheet)
-    '''
-    Or create by google sheet 
-    '''
-    sheet = gc.spreadsheets().create(body={
-        'properties': {
-            'title': 'a new spreadsheet',
-        },
-        'sheets':[
-            {
-                'properties': {
-                    'title': '',
-                    'index': 0,
-                    'gridProperties': {
-                        'rowCount': 4,
-                        'columnCount': 4
-                    }
-                },
-                "data": [
-                    {
-                        'startRow': 0,
-                        'startColumn': 0,
-                        'rowData': [
-                            {
-                                'values': [
-                                    {
-                                        'userEnteredValue': {
-                                            'numberValue': 0
-                                        }
-                                    },
-                                    {
-                                        'userEnteredValue': {
-                                            'numberValue': 1
-                                        }
-                                    },
-                                    {
-                                        'userEnteredValue': {
-                                            'numberValue': 2
-                                        }
-                                    },
-                                    {
-                                        'userEnteredValue': {
-                                            'numberValue': 3
-                                        }
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ],
-            }
-        ]
+    sheet = gd.files().create(body={
+        'name': 'a new spreadsheet',
+        'parents': [folder['id']],
+        'description': 'a example worksheet',
+        'mimeType': "application/vnd.google-apps.spreadsheet"
     }).execute()
-    print(sheet)
-    '''
-    then move to parenet folder
-    '''
-    sh = gd.files().get(
-        fileId=sheet['spreadsheetId']
-    ).execute()
-    sh['addParents'] = [folder['id']]
-    print(sh,)
-    request = gd.files().update(
-        fileId=sheet['spreadsheetId'],
-        body=sh
-    )
-    print(request)
-    response = request.execute()
-    print(response)
+    pprint(sheet)
 
+    # add a target worksheet
+    ws = gc.spreadsheets().batchUpdate(
+        spreadsheetId=sheet['id'],
+        body={
+            'requests': [
+                {
+                    'addSheet': {
+                        'properties': {
+                            'index': 0,
+                            'title': 'Deposits',
+                            'gridProperties': {
+                                'rowCount': 20,
+                                'columnCount': 12
+                            },
+                            'tabColor': {
+                                'red': 1.0,
+                                'green': 0.3,
+                                'blue': 0.4
+                            }
+                        },
+                    },
+                    'appendCells': {
+                        'sheetId': 0,
+                        'rows': {
+                            'values': [
+                                {
+                                    'userEnteredValue': {
+                                        'numberValue': 0
+                                    }
+                                },
+                                {
+                                    'userEnteredValue': {
+                                        'numberValue': 1
+                                    }
+                                },
+                                {
+                                    'userEnteredValue': {
+                                        'numberValue': 2
+                                    }
+                                },
+                                {
+                                    'userEnteredValue': {
+                                        'numberValue': 3
+                                    }
+                                },
+                            ]
+                        },
+                        'fields': '*'
+                    }
+                }
+            ]
+        });
+    response = ws.execute()
+    pprint(response)
 
     # Update WorkSheet Data
-    body = {
-        'range': 'A1:D2',
-        'majorDimension': 'ROWS',
-        'values': [
-            [1, 2, 3, 4],
-            [5, 6, 7, 8],
-        ]
-    }
-    response = gc.spreadsheets().values().update(
-        spreadsheetId=sheet['spreadsheetId'],
-        valueInputOption='RAW',
-        range='A1:D2',
-        body=body
-    ).execute()
-    print(response)
+    # body = {
+    #     'range': 'A1:D2',
+    #     'majorDimension': 'ROWS',
+    #     'values': [
+    #         [1, 2, 3, 4],
+    #         [5, 6, 7, 8],
+    #     ]
+    # }
+    # response = gc.spreadsheets().values().update(
+    #     spreadsheetId=sheet['id'],
+    #     valueInputOption='RAW',
+    #     range='A1:D2',
+    #     body=body
+    # ).execute()
+    # pprint(response)
 
 
     # Read Content of existing WorkSheet
@@ -164,7 +151,7 @@ def main():
         valueRenderOption='UNFORMATTED_VALUE',
         dateTimeRenderOption='FORMATTED_STRING')
     response = request.execute()
-    print(response)
+    pprint(response)
 
 if __name__ == '__main__':
     main()
