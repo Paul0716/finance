631415e9037eef5838f7f3d224f0ef7357fa33b9
Ku Hung-Hsi <justlove0714@hotmail.com>
2019-10-28 07:46:24 +0800

    arguments logic implment

--- main.py
+++ main.py
@@ -1,108 +1,26 @@
+from argparse import ArgumentParser
 from pprint import pprint
-from google_client.sheets import google_sheets
-from google_client.drive import google_drive
+from datetime import datetime
+from stock_report import crawler
 
-TARGET_FOLDER = 'Stock Report'
+def parse_argunments():
+    parser = ArgumentParser()
+    parser.add_argument('-s', '--start', help='crawl from specific date')
+    parser.add_argument('-e', '--end', help='crawl to specific date, default is today')
+    parser.add_argument('-b','--back', help='crawl specific date')
 
+    parser.print_help()
+    arguments = parser.parse_args()
+    return {
+        'start': datetime.strptime(arguments.start, '%Y-%m-%d') or None,
+        'end': datetime.strptime(arguments.start, '%Y-%m-%d') or None,
+        'back': int(arguments.back) if arguments.back else None,
+    }
 
 def main():
-    scope=[
-        'https://www.googleapis.com/auth/spreadsheets',
-        'https://www.googleapis.com/auth/drive'
-    ]
-    sheets_client = google_sheets(scope, path='./client_secret.json')
+    args = parse_argunments()
+    crawler.execute()
 
-    # google drive client
-    drive_client = google_drive(scope, path='./client_secret.json')
-
-    folder = drive_client.get_root_folder(TARGET_FOLDER)
-
-    # Create new WorkSheet
-    '''
-    create by google drive
-    '''
-    sheet = drive_client.create(body={
-        'name': 'a new spreadsheet',
-        'parents': [folder['id']],
-        'description': 'a example worksheet',
-        'mimeType': "application/vnd.google-apps.spreadsheet"
-    })
-
-    # add a target worksheet
-    response = sheets_client.addSheet(spreadsheetId=sheet['id'], body={
-        'properties': {
-            'index': 0,
-            'title': 'Deposits',
-            'gridProperties': {
-                'rowCount': 20,
-                'columnCount': 12
-            },
-            'tabColor': {
-                'red': 1.0,
-                'green': 0.3,
-                'blue': 0.4
-            }
-        },
-    })
-    pprint(response)
-
-    target = sheets_client.get(spreadsheetId=sheet['id'])
-
-    sheet_prop = sheets_client.find_sheet_by_name(spreadsheet_properties=target, sheet_name ='Deposits')
-    pprint(sheet_prop)
-
-    # Update WorkSheet Data
-    response = sheets_client.values_update(spreadsheetId=sheet['id'], valueInputOption='RAW', range='A1:D2', body={
-        'range': 'A1:D2',
-        'majorDimension': 'ROWS',
-        'values': [
-            [1, 2, 3, 4],
-            [5, 6, 7, 8],
-        ]
-    });
-    pprint(response)
-
-    # Apeend WorkSheetData
-    sheets_client.appendCells(spreadsheetId=sheet['id'], body={
-        'sheetId': sheet_prop['properties']['sheetId'],
-        'rows': [
-            {
-                'values': [
-                    {
-                        'userEnteredValue': {
-                            'numberValue': 1
-                        }
-                    },
-                    {
-                        'userEnteredValue': {
-                            'numberValue': 2
-                        }
-                    },
-                    {
-                        'userEnteredValue': {
-                            'numberValue': 3
-                        }
-                    }
-                ]
-            }
-        ],
-        'fields': '*'
-    })
-    pprint(response)
-
-    # Read Content of existing WorkSheet
-    # request = gc.spreadsheets().values().batchGet(
-    #     spreadsheetId=response['spreadsheetId'],
-    #     ranges='A1:D2',
-    #     valueRenderOption='UNFORMATTED_VALUE',
-    #     dateTimeRenderOption='FORMATTED_STRING')
-    # response = request.execute()
-    response = sheets_client.read(spreadsheetId=response['spreadsheetId'],
-        ranges='A1:D2',
-        valueRenderOption='UNFORMATTED_VALUE',
-        dateTimeRenderOption='FORMATTED_STRING')
-
-    pprint(response)
 
 if __name__ == '__main__':
     main()
--- stock_report/crawler.py
+++ stock_report/crawler.py
@@ -3,8 +3,9 @@
 
 import datetime
 from .utils import *
+from pprint import pprint
 
-class Crawler:
+class crawler:
 
     target = 'tsec'
 
@@ -17,8 +18,8 @@ class Crawler:
 
     def execute(self):
         print('execut')
-        stock_list = []
-        stock_list = get_stock_list('stocknumber.csv')
+        stock_list = ['0050']
+        # stock_list = get_stock_list()
         today = datetime.datetime.today()
         index = 0
 
@@ -27,14 +28,7 @@ class Crawler:
             stock_date = today - datetime.timedelta(days=4 * 365)
             index += 1
             while not (today.year == stock_date.year and stock_date.month > today.month):
-                print
-                stock_id, stock_date.year, stock_date.month
-                print
-                stock_id, today.year, today.month
+                pprint(stock_id, stock_date.year, stock_date.month)
+                pprint(stock_id, today.year, today.month)
                 stock_date = date_add_a_month(stock_date)
                 get_single_page(stock_id, str(stock_date.year), str(stock_date.month).zfill(2))
-
-
-
-
-
